4f99c86dd31320c9e6eb2d1598d68133b1888eae
Xavier Hernandez <43626114+xtriminioh@users.noreply.github.com>
2022-10-10 15:25:42 -0600

    Agregamos la declaracion que hace cumplir la inicializacion del test unitario.

--- Estructura/test_shopping_cart.py
+++ Estructura/test_shopping_cart.py
@@ -8,3 +8,11 @@ class TestShoppingCart(unittest.TestCase):
         price = 1.70
 
         self.new_product = Product(name,price)
+
+        #verificamos si los atributos que se crearon los tiene el nuevo producto
+        self.assertEqual(self.new_product.name, name)
+        self.assertEqual(self.new_product.price, price)
+
+
+if __name__ == '__main__':
+    unittest.main()

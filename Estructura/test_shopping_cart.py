371ef7a9ef600ce1fcde4e43bf497f7c142843cd
xtriminio <xtriminioh>
2022-10-12 11:49:04 -0600

    Metodos setUp y tearDown, para realizar acciones antes y despues de cada test.

--- Estructura/test_shopping_cart.py
+++ Estructura/test_shopping_cart.py
@@ -3,6 +3,17 @@ from product import Product
 
 class TestShoppingCart(unittest.TestCase):
 
+    #Metodos setup y teardown de TestCase
+    def setUp(self):
+        """"El metodo setUp se ejecuta antes de cada unas de las puebas."""
+        self.name = 'iPhone'
+        self.price = 500.00
+        self.smartphone = Product(self.name,self.price)
+
+    def tearDown(self):
+        """"El metodo tearDown se ejecuta despues de cada unas de las puebas."""
+        pass
+
     def test_product_object(self):
         name = 'Manzana'
         price = 1.70
@@ -13,5 +24,7 @@ class TestShoppingCart(unittest.TestCase):
         self.assertEqual(new_product.name, name, 'Lo sentimos, el nombre no es el mismo.')
         self.assertEqual(new_product.price, price, 'Lo sentimos, el precio no es el mismo.')
 
+    def test_product_name(self):
+        self.assertEqual(self.smartphone.name,self.name, 'Lo sentimos, el nombre del producto no es el mismo.') 
 
 if __name__ == '__main__': unittest.main()

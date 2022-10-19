60f174ffecf0e06099f8769b0940b0ed9cf445a0
Xavier Hernandez <43626114+xtriminioh@users.noreply.github.com>
2022-10-18 20:02:21 -0600

    "Pruebas con los Metodos de SetUp y tearDown"

--- Estructura/test_shopping_cart.py
+++ Estructura/test_shopping_cart.py
@@ -3,16 +3,29 @@ from product import Product
 
 class TestShoppingCart(unittest.TestCase):
 
+    def setUp(self):
+        """Este Metodo setup se ejecuta antes de cada una de las pruebas."""
+        self.name = 'iPhone'
+        self.price = 500.00
+
+        self.smartphone = Product(self.name, self.price)
+
+
+    def tearDown(self):
+        """Este Metodo teardown se ejeuta despues de cada una de ls pruebas."""
+
     def test_product_object(self):
         name = 'Manzana'
         price = 1.70
 
-        self.new_product = Product(name,price)
+        new_product = Product(name,price)
 
         #verificamos si los atributos que se crearon los tiene el nuevo producto
-        self.assertEqual(self.new_product.name, name)
-        self.assertEqual(self.new_product.price, price)
+        self.assertEqual(new_product.name, name)
+        self.assertEqual(new_product.price, price)
 
+    def test_prrduct_name(self):
+        self.assertEqual(self.smartphone.name, self.name)
 
 if __name__ == '__main__':
     unittest.main()
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 En este repositorio, esta hecho para llevar los apuntes, del curso de codigo facilito. De las Pruebas Unitarias
 
 ### Estructura de Proyectos
+En las estructura de los pryectos, tenes

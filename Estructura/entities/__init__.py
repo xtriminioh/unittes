1558576b6c2682b3351c122cb09fc09ac98bf565
xtriminio <xtriminioh>
2022-10-21 16:21:20 -0600

    Se realiza le exposicion de las clases.

--- Estructura/entities/__init__.py
+++ Estructura/entities/__init__.py
@@ -3,3 +3,7 @@
     Para que la carpeta sea tratada como un modulo en python,
     esta tiene que tener un archivo __init__.py dentro.
 """
+
+#exponemos la siguientes clases.
+from entities.product import Product
+from entities.shopping_cart import ShoppingCart

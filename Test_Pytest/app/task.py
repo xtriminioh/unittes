16a0f1a07e7b34bfa02cc7ec5dd37c881605dffa
Xavier <xtriminioh@gmail.com>
2022-10-31 21:31:16 -0600

    Se agregaron algunas propiedades a la clase Task y se crearon
    Pruebas para la misma.

--- Test_Pytest/app/task.py
+++ Test_Pytest/app/task.py
@@ -1,2 +1,6 @@
 class Task():
-    pass
+    def __init__(self, title, description, assigned_to, due_date):
+        self.title = title
+        self.description = description
+        self.assigned_to = assigned_to
+        self.due_date = due_date
--- Test_Pytest/app/test/test_task.py
+++ Test_Pytest/app/test/test_task.py
@@ -1,6 +1,19 @@
 import pytest
 
+from datetime import datetime
+from app.task import Task 
+
 class TestTask():
 
     def test_task(self):
         assert True
+
+    def test_new_task(self):
+        due_date = datetime.now()
+
+        task = Task('title','description','usuario',due_date)
+
+        assert task.title == 'title'
+        assert task.description == 'description'
+        assert task.assigned_to == 'usuario'
+        assert task.due_date == due_date
